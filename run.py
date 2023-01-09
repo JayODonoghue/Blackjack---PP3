fc0d1e35650d223f0df540795a817ea059809f6d
JAY O DONOGHUE <jay.odonoghue@hotmail.com>
2023-01-09 22:24:31 +0000

    Add get_username function using input data

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+print("BLACKJACK")
+
+
+def get_username():
+    """
+    Get username input from the user.
+    A while loop collects data from the user that will be a string
+    3 to 10 characters long 
+    """
+    while True:
+        print("Please enter your username\n")
+        print("Please make sure username is 3 - 10 characters")
+
+        username = input()
+
+        if len(username) >= 3:
+            print(f"Welcome {username}")
+            break
+            
+    return username
+
+
+get_username()
