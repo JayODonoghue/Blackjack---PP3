cef38ba034285c72b42bf47c60e5795957fc05b0
JAY O DONOGHUE <jay.odonoghue@hotmail.com>
2023-01-10 22:11:21 +0000

    Update get_username function spacing

--- run.py
+++ run.py
@@ -13,16 +13,16 @@ def get_username():
     A while loop collects data from the user that will be a string
     3 to 10 characters long 
     """
-    while True:
-        print("Please make sure your username is 3 - 10 characters")
+    print("Please make sure your username is 3 - 10 characters")
 
+    while True:    
         username = input("Please enter your username - ")
 
         if len(username) >= 3 and len(username) < 11:
             print(f"\nWelcome {username}!")
             break
         else:
-            print("Invalid username, please make is between 3-10 characters")
+            print("-------\nInvalid username, 3-10 characters required")
 
     return username
 
